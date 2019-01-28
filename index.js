df78d677d7597deddbb572666b050946b034dcaf
Josh Quintus <jquintus@attentivemobile.com>
2019-01-28 00:17:04 -0500

    Adding logic to download the PR body and check for empty checkboxes

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
-console.log("hello world")
+const https = require('https');
+const axios = require('axios');
+
 // Checks API example
 // See: https://developer.github.com/v3/checks/ to learn more
 module.exports = app => {
-  // app.on(['check_suite.*', 'check_run.*'], check)
-  app.on('check_suite', check)
+  app.on(['check_suite.requested', 'check_run.*'], check)
+  // app.on('check_suite', check)
 
   async function check (context) {
-    console.log("hello world")
+    const prUrl = context.payload.check_suite.pull_requests[0].url
+
+    console.log("=====================")
+    console.log(context.payload.action)
+    console.log(prUrl)
+    console.log("=====================")
+    // console.log(context)
     // Do stuff
     const { head_branch, head_sha } = context.payload.check_suite
 
-    // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
-    return context.github.checks.create(context.repo({
-      name: 'Checklist',
-      head_branch,
-      head_sha,
-      status: 'completed',
-      conclusion: 'success',
-      completed_at: new Date(),
-      output: {
-        title: 'Checklist!',
-        summary: 'All *items* checked'
+    axios.get(prUrl).then(resp => 
+    {
+      // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
+      body = resp.data.body
+      lines = body.split("\r\n");
+      
+      console.log("body:")
+      console.log(lines)
+
+      var matches = body.match("\[\s\s*\]")
+
+      if (matches)
+      {
+          console.log("failed!!!")
+          console.log(matches)
+          return context.github.checks.create(context.repo({
+            name: 'Checklist',
+            head_branch,
+            head_sha,
+            status: 'completed',
+            conclusion: 'failed',
+            completed_at: new Date(),
+            output: {
+              title: 'Checklist!',
+              summary: matches
+            }
+          }))
       }
-    }))
+      else
+      {
+          return context.github.checks.create(context.repo({
+            name: 'Checklist',
+            head_branch,
+            head_sha,
+            status: 'completed',
+            conclusion: 'success',
+            completed_at: new Date(),
+            output: {
+              title: 'Checklist!',
+              summary: 'All *items* checked'
+            }
+          }))
+      }
+    })
   }
 
   // For more information on building apps:
